d7bc6bfe4a7fa3f0d9d500605303ae4c59e21006
Jerome Wynne <J.J.Wynne47@gmail.com>
2017-04-25 20:54:03 +0100

    Full simulation for Ex 2.11 1 - Binomial sampling distribution with beta conjugate prior

--- BDA_003/BDA_003_2Ex11_1.R
+++ BDA_003/BDA_003_2Ex11_1.R
@@ -5,10 +5,42 @@
 # Less than 3 successes
 # What is the exact posterior density?
 
-# Prior distribution of parameter
+n_sim <- 5000
+n_trials <- 10
+n_max <- 2 # At most this many successes
 
+# Prior distribution of parameter, p(th)
+prior_params <- rbeta(n = n_sim, shape1 = 4, shape2 = 4)
+prior_params <- sort(prior_params, decreasing = FALSE)
+jpeg('Prior.jpg')
+hist(prior_params, breaks = 50, main = 'Prior', 
+     xlab = 'theta', probability = TRUE)
+dev.off()
 
-
-n_simulations <- 100
-
+# Sampling distribution, p(y|th)
+dsampling <- matrix(0, nrow = n_trials + 1, ncol = 1)
+for (k in c(0, seq(n_trials))) {
+  dsampling[k + 1, ] <- sum(dbinom(x = k, size = n_trials,
+                        prob = prior_params))
 }
+jpeg('Sampling.jpg')
+plot(c(0, seq(n_trials)), dsampling, type = 'h',
+     xlab = '# successes', ylab = 'Unscaled p(y|th)',
+     main = 'Sampling')
+dev.off()
+
+# Posterior distribution
+# Integrate p(y|theta)p(theta) 
+# over the observed results.
+# Our results state the number of successes
+# is y e {0, 1, .., n_max}.
+dposterior <- matrix(0, nrow = n_sim)
+for (k in c(0, seq(n_max))) {
+  dposterior <- dposterior + 
+                prior_params*dbinom(x = k, size = n_trials,
+                                    prob = prior_params)
+}
+jpeg('Posterior.jpg')
+plot(prior_params, dposterior, xlab = 'theta', 
+     ylab = 'p(theta|y)', main = 'Posterior')
+dev.off()
