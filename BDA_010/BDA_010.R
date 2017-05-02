bd93a7e4b3884d4dd7407bbb2a4f168d0e0f16d7
Jerome Wynne <J.J.Wynne47@gmail.com>
2017-05-02 19:03:40 +0100

    Censored and uncensored data in the exponential model

---
+++ BDA_010/BDA_010.R
@@ -0,0 +1,8 @@
+# BDA_010
+
+# Censored and uncensored data in the exponential model:
+# (a) Suppose y|theta is exponentially distributed with rate theta,
+#     and the marginal distribution of theta is Gamma(alpha, beta). Suppose
+#     we observe that y >= 10, but do not observe the exact value of y.
+#     What is the posterior distribution, p(theta|y >= 100), as a function of
+#     alpha and beta? Write down the posterior mean and vairance of theta.
